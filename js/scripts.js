317c0e286ea1d90ffcc94ba4ef90c7646aa283fe
Ryan Duff <ryanduff613@gmail.com>
2020-03-31 13:09:29 -0700

    create html form, gather user input with scripts, code working

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,14 @@
+$(document).ready(function(){
+  $('form').submit(function(event){
+    var userString = $("#originalString").val();
+    console.log(userString);
+    
+    
+    
+    
+    
+    event.preventDefault();
+  });
 
+});
   
