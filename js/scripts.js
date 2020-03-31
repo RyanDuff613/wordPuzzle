a6714cc75db27ec3c521a8101466b14521b3b5ba
Ryan Duff <ryanduff613@gmail.com>
2020-03-31 13:38:15 -0700

    create for loop, loop is working, struggling with .replace() method

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,15 @@
 $(document).ready(function(){
+
+  var vowels = ['a','e','i','o','u'];
   $('form').submit(function(event){
     var userString = $("#originalString").val();
     
-    var vowels = ['a','e','i','o','u'];
-    
-    
-    
-    
+    for (index = 0; index < vowels.length ;index +=1) {
+      userString.replace('a', '-');
+      console.log(userString);
+    }
+    userString.replace('a', '-');
     console.log(userString, vowels);
-    
-    
-    
-    
     
     event.preventDefault();
   });
