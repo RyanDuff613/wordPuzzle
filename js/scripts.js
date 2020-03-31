32b81e78af6a25eccfec7bb4dd410784db264e5d
Ryan Duff <ryanduff613@gmail.com>
2020-03-31 13:13:44 -0700

    add array called vowels containing aeiou

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,13 @@
 $(document).ready(function(){
   $('form').submit(function(event){
     var userString = $("#originalString").val();
-    console.log(userString);
+    
+    var vowels = ['a','e','i','o','u'];
+    
+    
+    
+    
+    console.log(userString, vowels);
     
     
     
